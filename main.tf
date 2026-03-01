91beb859617504fb0db97c3c8ed2c31edc0c803b
SiddharthaManthurthi <manthurthisiddhartha@gmail.com>
2026-03-01 11:23:47 +0530

    terraform

--- main.tf
+++ main.tf
@@ -8,7 +8,14 @@ resource "aws_vpc" "main" {
 
 resource "aws_internet_gateway" "main" {
     vpc_id = aws_vpc.main.id #vpc association
-
     tags = local.igw_final_tags
   
 }
+
+# #public Subnets
+# resource "aws_subnet" "public" {
+#     count = length(var.public_subnet_cidrs)
+#     vpc_id = aws_vpc.main.id
+#     cidr_block = var.public_subnet_cidrs[count.index]
+  
+# }
